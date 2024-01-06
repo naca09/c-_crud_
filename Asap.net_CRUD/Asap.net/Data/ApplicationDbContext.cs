9c76f6a21efd719fb207735297c4aa0123612f70
naca09 <86470016+naca09@users.noreply.github.com>
2024-01-07 02:24:23 +0700

    c#_CRUD

---
+++ Asap.net_CRUD/Asap.net/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Asap.net.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asap.net.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
